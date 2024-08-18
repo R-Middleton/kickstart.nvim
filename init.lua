47323dabae293df4d7df30f55b542f23f7696c19
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2024-08-18 15:23:19 +0100

    added vimtext plugin

--- init.lua
+++ init.lua
@@ -367,6 +367,16 @@ require('lazy').setup({
   },
 
   {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'skim'
+    end,
+  },
+
+  {
     'nvim-neotest/neotest',
     dependencies = { 'nvim-neotest/nvim-nio', 'nvim-neotest/neotest-jest' },
     opts = {
